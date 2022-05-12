eb462bde8f3a555ccc614e11533eba1b7100e82e
Felipe Fernandes <felipefernandesmiranda@gmail.com>
2022-05-12 07:57:34 -0300

    Add e config do MiragJS e Faker para integra api no front-end

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,16 +1,26 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
+import { Box, Button, Checkbox, Flex, Heading, Icon, Spinner, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
 import Link from "next/link";
+import { useEffect } from "react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { useQuery } from "react-query"
+
 import Header from "../../components/Header";
 import Pagination from "../../components/Pagination";
 import { Sidebar } from "../../components/SideBar";
+import { api } from "../../services/api";
+import { useUsers } from "../../services/hooks/useUsers";
 
 export default function UsersList(){
+  const { data, isLoading, error, isFetching } = useUsers()
+
+  
   const isWideVersion = useBreakpointValue({
     base: false,
     lg:true,
 
   })
+
+  
 
   return(
     <Box>
@@ -19,7 +29,11 @@ export default function UsersList(){
         <Sidebar/>
         <Box flex="1" borderRadius={8} bg="gray.800" p="8" >
           <Flex mb="8" justify="space-between" align="center">
-            <Heading size="lg" fontWeight="normal">Usuários</Heading>
+            <Heading size="lg" fontWeight="normal">
+              Usuários
+            
+              {!isLoading && isFetching && <Spinner size='sm' color='gray.500' ml='4'/>}
+            </Heading>
             <Link href="/users/create" passHref>
               <Button
                 as="a"
@@ -32,35 +46,52 @@ export default function UsersList(){
               </Button>
             </Link>
           </Flex>
-
-          <Table colorScheme="whiteAlpha" >
-            <Thead>
-              <Tr>
-                <Th px={["4","4","6"]} color="gray.300" width="8">
-                  <Checkbox colorScheme="pink"/>
-                </Th>
-                <Th>Usuários</Th>
-                { isWideVersion && <Th>Data de cadastro</Th> }
-                <Th width="8"></Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td px={["4","4","6"]}>
-                <Checkbox colorScheme="pink"/>
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Felipe Fernandes</Text>
-                    <Text fontSize="sm" color="gray.300">felipefmiranda@gmail.com</Text>
-                  </Box>
-                </Td>
-                { isWideVersion &&  <Td>04 de junho de 2021</Td> }
-                
-              </Tr>
-            </Tbody>
-          </Table>
-          <Pagination/>
+          {isLoading ? (
+            <Flex justify="center">
+              <Spinner/>
+            </Flex>
+          ) : error ? (
+            <Flex justify="center">
+              <Text>Falaha ao carregar dados dos usuários</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorScheme="whiteAlpha" >
+                <Thead>
+                  <Tr>
+                    <Th px={["4","4","6"]} color="gray.300" width="8">
+                      <Checkbox colorScheme="pink"/>
+                    </Th>
+                    <Th>Usuários</Th>
+                    { isWideVersion && <Th>Data de cadastro</Th> }
+                    <Th width="8"></Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {data.map(user=>{
+                    return(
+                      <Tr key={user.id}>
+                        <Td px={["4","4","6"]}>
+                        <Checkbox colorScheme="pink"/>
+                        </Td>
+                        <Td>
+                          <Box>
+                            <Text fontWeight="bold">{user.name}</Text>
+                            <Text fontSize="sm" color="gray.300">{user.email}</Text>
+                          </Box>
+                        </Td>
+                        { isWideVersion &&  <Td>{user.createdAt}</Td> }
+                        
+                      </Tr>
+                    )
+                  })}
+                </Tbody>
+              </Table>
+              <Pagination/>
+            </>
+          )
+        }
+          
         </Box>
       </Flex>
     </Box>
